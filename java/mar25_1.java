29aa8cfe55a6b4c001f53cdf0a4669a4a094bde9
sasank <kotasasank123@gmail.com>
2026-03-28 09:28:08 +0530

    polymorphism

--- java/mar25_1.java
+++ java/mar25_1.java
@@ -6,7 +6,7 @@ class Insta{
     private String username="sasank";
     private String pass="123";
     private String mobile="12345";
-
+    
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
@@ -25,20 +25,81 @@ class Insta{
     public String getMobile() {
         return mobile;
     }
+    int generateOtp(){
+        int otp=1000+(int)(Math.random()*8999);
+        System.out.println("the otp is"+otp);
+        return otp;
+    }
+    Insta(String username, String pass, String mobile ){
+        this.mobile=mobile;
+        this.pass=pass;
+        this.username=username;
+    }
+    Insta(){
 
+    }
 
 }
 public class mar25_1 {
-    Scanner sc= new Scanner(System.in);
-    Insta user= new Insta();
+   static  Scanner sc= new Scanner(System.in);
+    static Insta user= new Insta();
+    void signup(){
+        System.out.println("Enter details to create a account ");
+        System.out.println("Enter your good name");
+        String name=sc.next();
+        System.out.println("enter your username");
+
+        String username=sc.next();
+        System.out.println("Enter your password");
+        String pass=sc.next();
+        System.out.println("Enter mobile number");
+       String mobile=sc.next();
+       int otp=user.generateOtp();
+       while (true) {
+        // System.out.println("the otp is ");
+        System.out.println("enter the otp");
+        int o=sc.nextInt();
+        if(o==otp){
+
+            Insta obj =new Insta(username,pass, mobile);
+            System.out.println("account created successfully");
+
+            System.out.println("enter 1 to login or else 2 to exit");
+            int c=sc.nextInt();
+            if(c==1){
+                login();
+                break;
+            }
+            else{
+                System.out.println("thank you");
+                break;
+            }
+        }
+        else{
+            System.out.println("Invalid otp");
+            System.out.println("1 to re enter \n 2 to resend \n 3 to exit ");
+            int c=sc.nextInt();
+            if(c==2){
+                otp=user.generateOtp();
+
+            }
+            else if(c==3){
+                System.out.println("thank you");
+                break;
+            }
+
+        }
+       }
+    }
     void login(){
+        
         System.out.println("enter the login details \n username");
 
         String username=sc.next();
         String pass= sc.next();
-
+        int otp=(int)(Math.random()*1000);
         if(user.getUsername().equals(username) && user.getPass().equals(pass)){
-            System.out.println("Login successful");
+            System.out.println("Login successfull");
         }
         else if(user.getUsername().equals(username)){
             System.out.println("if you want to reset your password press 1 else press 2");
@@ -63,26 +124,56 @@ public class mar25_1 {
         }
         else
         {
+            while (true) {
+                
+            
             System.out.println("wanted to know your username? then enter your mobile number if not enter 'no'");
             String mobile=sc.next();
             if(user.getMobile().equals(mobile)){
                 System.out.println("your username is "+user.getUsername());
+                System.out.println("enter 1 to login or else 2 to exit");
+                int n=sc.nextInt();
+                if(n==1){
+                    login();
+                    break;
+                }
+                else if(n==2){
+                    System.out.println("thank you");
+                    break;
+                }
+                else{
+                    System.out.println("Inavlid Input");
+                }
+
             }
             else{
                 System.out.println("user not found");
                 System.out.println("enter 1 to re enter the mobile number \n 2 to exit");
                 int c=sc.nextInt();
                 if(c==1){
-                    login();
+                    // login();
+                    break;
                 }
-                
+                else{
+                    System.out.println("thank you");
+                break;
+                }
             }
         }
-
+        }
     }
     public static void main(String[] args) {
     //    Insta user=new Insta();
+    Scanner sc= new Scanner(System.in);
         mar25_1 obj= new mar25_1();
+        System.out.println("HI welcome .. press \n 1.to Signup \n 2. to Login");
+      int c= sc.nextInt();
+      if(c==1){
+        obj.signup();
+      }
+      else if(c==2)
         obj.login();
+    else 
+        System.out.print("Invalid input");
     }
 }
---
+++ java/mar28_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class A{
+    Scanner sc= new Scanner(System.in);
+    int m1(int a){
+        System.out.println(a);
+        return sc.nextInt();
+    }
+    int m2(int a,int b){
+        System.out.println(a+" "+b);
+        return a+b;
+    }
+
+}
+class mar28_1 extends A{
+    int m1(int a){
+        System.out.println("in overriden class");
+        System.out.println(super.m1(sc.nextInt()));
+        return sc.nextInt();
+    }
+    public static void main(String[] args) {
+         mar28_1 obj=new mar28_1();
+      
+       System.out.println(obj.m1(obj.sc.nextInt())); 
+       System.out.println(obj.m2(obj.sc.nextInt(), obj.sc.nextInt())); 
+        
+        
+    }
+}
