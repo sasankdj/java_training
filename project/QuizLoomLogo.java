f4eb7f5f5e869a7e9997a3bc330a1c0d64052df4
sasank <kotasasank123@gmail.com>
2026-05-03 23:02:00 +0530

    project phase 5

---
+++ project/QuizLoomLogo.java
@@ -0,0 +1,85 @@
+// QuizLoom ASCII Art - Console Based Java Program with Colors
+// Source ASCII from user file: :contentReference[oaicite:0]{index=0}
+
+public class QuizLoomLogo {
+    // ANSI Color Codes
+    public static final String RESET = "\u001B[0m";
+
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
+    // Bright Colors
+    public static final String BRIGHT_BLUE = "\u001B[94m";
+    public static final String BRIGHT_PURPLE = "\u001B[95m";
+    public static final String BRIGHT_CYAN = "\u001B[96m";
+    public static final String BRIGHT_YELLOW = "\u001B[93m";
+    public static final String BRIGHT_MAGENTA = "\u001B[95m";
+
+    public static void main(String[] args) {
+
+        // Logo Symbol
+        System.out.println(BRIGHT_PURPLE + 
+"                                                            mda0099990bj");
+        System.out.println(BRIGHT_PURPLE + 
+"                                                         faaa0099988887766c");
+        System.out.println(BRIGHT_PURPLE + 
+"                                                      yd009099998887776665557x");
+        System.out.println(BRIGHT_PURPLE + 
+"                                                     m88888888jox  yok66555444j     x05e");
+        System.out.println(BRIGHT_BLUE + 
+"                                                    f777667d            b444333f   755i");
+        System.out.println(BRIGHT_BLUE + 
+"                                                   o665556     uXUUWu     422222f  xt");
+        System.out.println(BRIGHT_BLUE + 
+"                                                   554444s   wTTTTSSSSt   z111113");
+        System.out.println(BRIGHT_CYAN + 
+"                                                  l333334    cSTy  jRRj    8ZZZZZo  zjjlmny");
+        System.out.println(BRIGHT_CYAN + 
+"                                                  h222216        s2RQQ     dZZZZZo  zpoprsz");
+        System.out.println(BRIGHT_CYAN + 
+"                                                  t111111       vQQYy      ZYYYYYr");
+        System.out.println(BRIGHT_YELLOW + 
+"                                                   2ZZZYYe       uv       9XXXXX1  xqx");
+        System.out.println(BRIGHT_YELLOW + 
+"                                                   zZYYYXX2     sPPp y0997YUVWWY   pcccr");
+        System.out.println(BRIGHT_BLUE + 
+"                                                    vXXXXWWW1y      z0988776YUUs     wff");
+        System.out.println(BRIGHT_BLUE + 
+"                                                      3XWWWWVVVUTTTRPZ87766544f");
+        System.out.println(BRIGHT_PURPLE + 
+"                                                       vYWVVVVUUUUTRPONV65443322d");
+        System.out.println(BRIGHT_PURPLE + 
+"                                                          kXUVVUUTTSRQPNQf5322211m");
+        System.out.println(BRIGHT_PURPLE + 
+"                                                              rmgccfkq     p31111w");
+        System.out.println(WHITE + 
+"                                                                              y");
+
+        // Text QUIZLOOM
+        System.out.println(BLUE + 
+"                             kQFFFRl               dFHy           cas");
+        System.out.println(BLUE + 
+"                           KFFGMRMGFFZ              U5            22m");
+        System.out.println(BLUE + 
+"                          NFG      rFFL  KJs   JJZ qJK  PJJJJJJc  11m        y42229     z3333a   v42 533f g335q");
+        System.out.println(PURPLE + 
+"                         oFFi       9FFl FFm   FFR mFGy okllJFFp  ZZm       3ZZiw0ZZe  41Zgx011d u211qo2116ue22c");
+        System.out.println(PURPLE + 
+"                         uFF0    bl TFGo FFn   FFR mFGy   vKFY    YYl      gY1    vZ1yjZ3    wZ1zu21z  h11   422");
+        System.out.println(BRIGHT_PURPLE + 
+"                          1EF2  sMFFFFY  GFc  rFFR mFGy  VFIq     XXk      pXXz   bY3 rYYx   eZ2 t1Z   h11   422");
+        System.out.println(BRIGHT_BLUE + 
+"                           lKFFFFFFEEJq  XFFFFFFFR mFGyrHFFFFGFL  WWWWWWWWw fXXXXWX1x  iYYYYYY1w tZZz  h11   422");
+        System.out.println(CYAN + 
+"                              zb2b  kIF4   l0x  z   z    zzzzzz                ohw        qiv");
+
+        // Footer
+        System.out.println("\n" + BRIGHT_YELLOW + "                         QUIZLOOM - Learn, Play, Grow!" + RESET);
+    }
+}
